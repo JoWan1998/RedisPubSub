3470245188220978df893cd21729bd55839e36e2
Jose Wannan <2993999310101@ingenieria.usac.edu.gt>
2021-05-02 20:11:35 -0600

    as

--- Sub/go.mod
+++ Sub/go.mod
@@ -2,4 +2,8 @@ module github.com/JoWan1998/RedisPubSub/Sub
 
 go 1.16
 
-require github.com/go-redis/redis/v8 v8.8.2
+require (
+	github.com/go-redis/redis/v7 v7.4.0
+	github.com/onsi/ginkgo v1.15.0 // indirect
+	github.com/onsi/gomega v1.10.5 // indirect
+)
--- Sub/subscriber.go
+++ Sub/subscriber.go
@@ -11,10 +11,10 @@ func main() {
 	svc := redis.NewClient(&redis.Options{
 		Addr: "127.0.0.1:6379",
 	})
-	sub = svc.pubsub()
+	sub := svc.pubsub()
 	sub.subscribe("mensaje")
 	for {
-		msg = sub.get_message()
+		msg := sub.get_message()
 		log.Println("Message: ", msg)
 	}
 
